591e33a1f2dbc5d05b4021fa879236999fd56038
Maxine Whitely <m.whitely16@gmail.com>
2020-04-08 12:24:46 -0500

    more code commenting

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # Scaffolded Exercises
-## Local Variable Tracking / State Management
-- using abstract syntax trees (ASTs) to analyze / modify source code
-    - [explore an AST without downloading anything](https://astexplorer.net/)
-#### `rewriteCode.js`
-Deanonymizes functions
 
-#### `managaeStateManager.js`
-Creates state manager
+## Local Variable Tracking / State Management
+
+-   using abstract syntax trees (ASTs) to analyze / modify source code
+    -   [explore an AST without downloading anything](https://astexplorer.net/)
+
+#### `rewriteCode.js`
+
+Deanonymizes functions so that we can create a model of the nested scope structure of the program. This is important for creating the state manager because we don't want two variables with the same named that are defined in different scopes to overwrite each others' values.
+
+#### `createStateManager.js`
+
+Creates state manager object and adds lines to the original source code in order to allow for state manager object updates in real-time.
 
 ---
+
 ### Future considerations / questions
-- how to handle ASTs for programs with multiple JS files?
+
+-   how to handle ASTs for programs with multiple JS files?
--- createStateManager.js
+++ createStateManager.js
@@ -1,6 +1,5 @@
 /*
-    Traverse through a program's AST and create a stateManager with an entry for each variable
-    in the program.
+    Traverse through a program's AST and create a stateManager with an entry for each variable in the program. Insert code in the original source code that updates the state manager whenever a variable value is changed.
 */
 
 // to handle typescript error :https://stackoverflow.com/questions/41292559/could-not-find-a-declaration-file-for-module-module-name-path-to-module-nam
@@ -8,19 +7,21 @@ const estraverse = require("estraverse");
 const recast = require("recast");
 const stringify = require("json-stringify-safe");
 const fs = require("fs");
+const path = require("path");
 
-/* ************************ Setup ******************************* */
-
-/* Initialize state manager */
+/* Initialize state manager. This will be inserted into the source code at the very end of its creation/the source code's modification (see scriptUpdates array comment) and will be the definitive source for every variable's value in the program. */
 let stateManager = {};
 
-/* Initialize scope collector */
-let scopeList = []; // TODO: RENAME VAR
+/* Initialize scope collector. This will be an array of arrays where the 0th item in each entry is the current scope's nested scope. */
+let programScopes = [];
+
+/* Initialize script update collector. This will be an array of objects representing the lines to be added to the original source code in order to update the stateManager in real time. Each entry will have the location that the update should go as well as the update itself */
+let scriptUpdates = [];
 
 /* turn on/off debug printing */
 let printSpecialPrint = true;
 
-/* Special console log formatting */
+/* Special printing formatting */
 const spprint = function (caller, info, isObj = false) {
     if (printSpecialPrint) {
         let specialPrint = `~~~~~~${caller}:`.padEnd(35, " ");
@@ -34,52 +35,48 @@ const spprint = function (caller, info, isObj = false) {
 };
 
 const fileKey = "xkcd_src";
-const scriptString = fs.readFileSync(`./updated_${fileKey}.js`).toString();
+const scriptString = fs
+    .readFileSync(path.resolve(__dirname, `./updated_${fileKey}.js`))
+    .toString();
 const ast = recast.parse(scriptString);
-
-/* ********************** End setup ***************************** */
 
 /* referenced: https://github.com/airportyh/esprima_fun/blob/master/scope_chain.js. Creating a list of scopes where 0th item in each scope list is the function name or null if function is anonymous. Initialized with global 'Program' scope */
 let scopeChain = [["Program"]];
 
-/* will be an array of objects, each with the location of the update in the source code + the update itself */
-let scriptUpdates = [];
-
+/* traverse the AST */
 estraverse.traverse(ast.program, {
     enter: enter,
     leave: leave
 });
-console.log(stateManager);
-console.log(scopeList);
+spprint("traversal results", stateManager);
+spprint("traversal results", programScopes);
 
 let stateManagerStr = JSON.stringify(stateManager);
-fs.writeFileSync(`./state_manager_${fileKey}.json`, stateManagerStr);
 
-let scopeListStr = JSON.stringify(scopeList);
-fs.writeFileSync(`./scope_list_${fileKey}.json`, scopeListStr);
-
-function addStateManagerUpdates(source, scriptUpdates, stateManagerStr) {
-    /* sort in descending order so we are modifying the file from the end to the beginning */
-    scriptUpdates = scriptUpdates.sort((a, b) => b["loc"] - a["loc"]);
-    let sourceArr = source.split("\n");
-    for (let update of scriptUpdates) {
-        const locs = update["loc"].split(".");
-        const line = locs[0];
-        // const col = locs[1];
-        sourceArr.splice(line, 0, update["autoStr"]);
-    }
-    /* add state manager object to top of file */
-    return `\n/* autogen added */ \nlet stateManager = ${stateManagerStr}\n/* end autogen added */\n\n${sourceArr.join(
-        "\n"
-    )}`;
-}
-
+/* update the source code with the state manager */
 const finalSource = addStateManagerUpdates(
     scriptString,
     scriptUpdates,
     stateManagerStr
 );
 fs.writeFileSync(`./final_${fileKey}.js`, finalSource); // @TODO: should these modifications be on the orig script so that anonymous functions are still accurately represented? or could re-anonymize functions at the very end of this so that the script runs in the exact same way it did before just with the state manager added
+
+/* after traversing through the source code's AST representation and collecting information about the places where variable updates are made, modify the source code so that it updates the stateManager every time a variable gets updated. */
+function addStateManagerUpdates(source, scriptUpdates, stateManagerStr) {
+    /* sort in descending order so we are modifying the file from the end to the beginning so we don't mess up the recorded locations for updates downstream */
+    scriptUpdates = scriptUpdates.sort((a, b) => b["loc"] - a["loc"]);
+    let sourceArr = source.split("\n");
+    for (let update of scriptUpdates) {
+        const locs = update["loc"].split(".");
+        const line = locs[0];
+        /* insert the update into the correct position */
+        sourceArr.splice(line, 0, update["autoStr"]);
+    }
+    /* compile the updated source code and add the state manager object to it */
+    return `\n/* autogen added */ \nlet stateManager = ${stateManagerStr}\n/* end autogen added */\n\n${sourceArr.join(
+        "\n"
+    )}`;
+}
 
 function enter(node) {
     let scopedFxnName = null;
@@ -117,28 +114,38 @@ function enter(node) {
         addVarsToStateManager(node, currentScope);
     }
     if (isVariableUpdate(node)) {
-        // let endLoc = meta.end.offset;
+        /* nodes in recast tree have location objects that specify their location by line and column in the source code. This creates unique location keys from the given information. */
         const endLoc = node.loc.end;
         const locKey = `${endLoc["line"]}.${endLoc["column"]}`;
 
         /* find var in state manager */
         let nodeName = getVarName(node);
+        // @TODO: make sure that this still works when you're updating the
+        // value of a variable that is outside of your current scope (ex:
+        // updating the value of `position` in the drag function...)
         let stateManagerKey = `${currentScope[0]}:${nodeName}`;
         /* add code in src to update the state manager */
         const updateStr = `\n/* autogen added */ \nstateManager["${stateManagerKey}"] = ${nodeName}\n`;
 
-        scriptUpdates.push({ loc: locKey, autoStr: updateStr });
+        /* add updates to the update collector so that we can insert all the updates to the source code at the same time at the end of the traversal */
+        scriptUpdates.push({
+            loc: locKey,
+            autoStr: updateStr
+        });
     }
 }
 
+/** if we're leaving a node that created a new scope, update the scopeChain so that
+ * the current scope is always represented by the last item in the scopeChain array. */
 function leave(node) {
     if (createsNewScope(node)) {
         let currentScope = scopeChain.pop();
         printScope(currentScope, node);
-        scopeList.push(currentScope);
+        programScopes.push(currentScope);
     }
 }
 
+/** pretty printing for scope information */
 function printScope(scope, node) {
     const declaredVars = scope.slice(1);
     const varsDisplay =
@@ -171,6 +178,9 @@ function isVariableUpdate(node) {
     );
 }
 
+/** the name of a variable is located in different spots depending on
+ * the AST node type. This helper function accesses the name of a variable
+ * depending on the node type. */
 function getVarName(node) {
     let varName = null;
 
@@ -207,6 +217,8 @@ function createsNewScope(node) {
     );
 }
 
+/** add function inputs to the state manager object. The changing values of
+ * inputs needs to be tracked as well as the changing values of variables! */
 function addInputsToStateManager(node, fxnName) {
     const params = isAnonymizedFunction(node) ? node.init.params : node.params;
 
