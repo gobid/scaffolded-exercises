19440caa32ee9cfe2d112d318d2db48bca0d505f
Maxine Whitely <m.whitely16@gmail.com>
2020-06-11 17:23:37 -0500

    cleanup before more code breakdown

--- findInitializerCode.js
+++ findInitializerCode.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
+/** Load in data collected from instrumented code */
 const timestampKey = "1591811153191";
 const stateManager = require(path.resolve(__dirname, `./exercise-data/${timestampKey}/stateManager.json`));
 const callCounts = require(path.resolve(__dirname, `./exercise-data/${timestampKey}/callCounts.json`));
@@ -19,7 +20,7 @@ let runCode = allSnippets.slice(domContentLoadedAt + 1);
 let domObjects = [];
 for (let key in stateManager) {
     if (stateManager[key][0] === true) {
-         /* key looks like 'Program:Map:$overlay'. Need to get the last item, which is the 
+        /* key looks like 'Program:Map:$overlay'. Need to get the last item, which is the
          * DOM object name */
         const domObjNameArr = key.split(":");
         const domObjName = domObjNameArr[domObjNameArr.length - 1];
@@ -27,31 +28,37 @@ for (let key in stateManager) {
     }
 }
 
-/** Find out the most recently run snippet (the latest in the array) to have been run that modifies
+let firstExerciseInfo = getFirstExercise(allSnippets, domObjects);
+console.log(firstExerciseInfo);
+let firstExerciseInitCode = getInitializerCode(firstExerciseInfo.domObj, initCode, callCounts);
+console.log(firstExerciseInitCode);
+
+/** @TODO Find out the most recently run snippet (the latest in the array) to have been run that modifies
  * a DOM object.
- * DONE BELOW: INTERMEDIATE STEP TODO FIRST: find the most recenty run snippet that includes a DOM object */
-let firstExercise = null;
-let firstExerciseLoc = null;
-let firstExerciseDomObj = null;
-for (let s = allSnippets.length  - 1; s >= 0; s--) {
-    for (let o = 0; o < domObjects.length; o++) {
-        if (allSnippets[s].includes(domObjects[o])) {
-            firstExercise = allSnippets[s];
-            firstExerciseLoc = s;
-            firstExerciseDomObj = domObjects[o];
-            break;
+ * @TODO how to know if something uses or modifies a DOM object?
+ * DONE BELOW: find the most recenty run code snippet that includes a DOM object */
+function getFirstExercise(snippets, domObjects) {
+    let exerciseInfo = {};
+    for (let s = snippets.length - 1; s >= 0; s--) {
+        for (let o = 0; o < domObjects.length; o++) {
+            if (snippets[s].includes(domObjects[o])) {
+                exerciseInfo["code"] = snippets[s];
+                exerciseInfo["arrayLoc"] = s;
+                exerciseInfo["domObj"] = domObjects[o];
+                return exerciseInfo;
+            }
         }
     }
-    /** Need to break out of both for-loops once the first exercise is assigned */
-    if (firstExercise !== null) {
-        break;
-    }
 }
-console.log(firstExercise)
 
-
-/** input: string name of dom object, initializer code array.
- * Output: all setup code that touches the domObj in question
+/** Input: string name of dom object, initializer code array.
+ * Output: all setup code that touches the domObj in question as long
+ * as it is only run once
+ * @TODO run # may be a shaky metric to generalize because
+ * what if you need to run the same setup code on different elements? For example, if image
+ * loading onto the map only happened at the beginning of the site loading (but still loaded
+ * nine images) the image loading setup code would still not be included. That feels like a
+ * problem.
  */
 function getInitializerCode(domObj, initCode, callCounts) {
     let currObjInitCode = [];
@@ -66,9 +73,6 @@ function getInitializerCode(domObj, initCode, callCounts) {
     return currObjInitCode;
 }
 
-let firstExerciseInitCode = getInitializerCode(firstExerciseDomObj, initCode, callCounts);
-console.log(firstExerciseInitCode);
-
 /** in the runLog, code snippets are added with their associated key as a comment so that 
  * we can get the information we've stored about that code using the key. Example:
  *  '/* $map.append($image);:116:116 * / \n' 
@@ -80,8 +84,6 @@ function parseCodeKey(snippet) {
     let key = keyComment.split("*")[1]; /** ['/', ' $map.append($image);:116:116 ', '/' ] */
     return key.trim();
 }
-
-
 
 /** TODO MAYBE: Create an object whose keys are the same as the callCounts key and values is the
  * corresponding code snippets index in the array AND the code snippet itself. Example:
