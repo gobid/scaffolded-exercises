05012c19d5118fcc9b0df555cf773969b7727017
Gobi Dasu <gobi@gobidasu.com>
2023-04-17 14:37:40 -0500

    Get to parse in JS var tracking

--- ex_info_to_ex.py
+++ ex_info_to_ex.py
@@ -1,17 +1,31 @@
 # python3 ex_info_to_ex.py
 import json
 import pprint
+import esprima
 
 # import examples' source
-xkcd_js_src = open("temp/xkcd_src.js")
+xkcd_js_src = open("temp/xkcd_src.js").read()
 
 fi = open("ordering.js")
 ordering = json.loads(fi.read())
-
 # pprint.pprint(ordering)
+
+def modify_js_to_track_vars(src_code, vars_to_track):
+    t = esprima.parseScript(src_code)
+    # print(t)
+    
+
+
+    return src_code
 
 for i, ex in enumerate(ordering):
     print("Writing ex:", str(i))
+    vars_to_track = [ex.domObj]
+    for oe in ex.otherElemsIncluded:
+        vars_to_track.push(oe)
+    for v in ex.variables:
+        vars_to_track.push(v)
+
     # pprint.pprint(ex)
     eg = open("../scaffolded-exercises-interface/src/pages/auto-exercise" + str(i) + ".js", "w+") # path to SEI
     eg.write("""import React from 'react';
@@ -21,7 +35,7 @@ window.$ = $;
 
 export default class ExerciseAG""" + str(i) + """ extends React.Component {
     componentDidMount() {
-        """ + xkcd_js_src + """
+        """ + modify_js_to_track_vars(xkcd_js_src, vars_to_track) + """
     }
 }
     """)
