67ee84b34e8526f3e35876935b66118226ed3f73
Gobi Dasu <gobi@gobidasu.com>
2023-04-19 12:41:47 -0500

    Autogen js for SEI but need to debug few things

--- ex_info_to_ex.py
+++ ex_info_to_ex.py
@@ -2,6 +2,7 @@
 import json
 import pprint
 import esprima # esprima==4.0.1
+import escodegen # escodegen==1.0.11, esutils==1.0.1
 
 # import examples' source
 xkcd_js_src = open("temp/xkcd_src.js").read()
@@ -10,13 +11,20 @@ fi = open("ordering.js")
 ordering = json.loads(fi.read())
 # pprint.pprint(ordering)
 lines_to_splice_in = [] # global variable
+global_depth = 0 # for debugging
 
 def instrument(t, vars_to_track):
+    global global_depth # for debugging
+    global_depth += 1
+    # print("global_depth", global_depth)
+    if hasattr(t, "type"):
+        print("t type: ", t.type)
+        # print(escodegen.generate(t)[:100])
     if hasattr(t, "body"):
-        print("t type: ", t.type, "going into body")
+        # print("t has a body, going in")
         instrument(t.body, vars_to_track)
     if isinstance(t, list):
-        print("t is a list")
+        # print("t is a list")
         for body_elem in t:
             # print("body_elem:", body_elem)
             instrument(body_elem, vars_to_track)
@@ -27,12 +35,14 @@ def instrument(t, vars_to_track):
         if t.expression and t.expression.left:
             if t.expression.left.name in vars_to_track:
                 print("need to track", t.expression.left.name, "at", t.expression.left.loc)
+                lines_to_splice_in.append({"variable": t.expression.left.name, "line": t.expression.left.loc.end.line})
         instrument(t.expression, vars_to_track)
     if hasattr(t, "declarations"): # variable declaration
         if t.declarations:
             for decl in t.declarations:
                 if decl.id.name in vars_to_track:
                     print("need to track", decl.id.name, decl.id.loc)
+                    lines_to_splice_in.append({"variable": decl.id.name, "line": decl.id.loc.end.line})
                 if decl.init.type == "FunctionExpression":
                     instrument(decl.init, vars_to_track)
     if hasattr(t, "alternate"): # else statement
@@ -41,33 +51,54 @@ def instrument(t, vars_to_track):
     if hasattr(t, "callee"): # obj.method() could modify object
         if hasattr(t.callee, "object"):
             if hasattr(t.callee.object, "callee"):
-                if t.callee.object.callee.name in vars_to_track:
+                if t.callee.object.callee and t.callee.object.callee.name in vars_to_track:
                     print("need to track", t.callee.object.callee.name, t.callee.object.callee.loc)
-            else:
-                if t.callee.object.name in vars_to_track:
-                    print("need to track", t.callee.object.name, t.callee.object.loc)
+                    lines_to_splice_in.append({"variable": t.callee.object.callee.name, "line": t.callee.object.callee.loc.end.line})
+                else:
+                    if t.callee.object and t.callee.object.name in vars_to_track:
+                        print("need to track", t.callee.object.name, t.callee.object.loc)
+                        lines_to_splice_in.append({"variable": t.callee.object.name, "line": t.callee.object.loc.end.line})
     if hasattr(t, "left"):
         if t.left:
             if hasattr(t.left, "object"):
-                if t.left.object.name in vars_to_track:
-                    print("need to track", t.left.object.name)
+                if t.left.object and t.left.object.name in vars_to_track:
+                    print("need to track", t.left.object.name, t.left.object.loc)
+                    lines_to_splice_in.append({"variable": t.left.object.name, "line": t.left.object.loc.end.line})
+    if hasattr(t, "init"):
+        if t.init:
+            instrument(t.init, vars_to_track)
+    if hasattr(t, "arguments"): # cover async functions
+        instrument(t.arguments, vars_to_track)
+    global_depth -= 1
 
 def modify_js_to_track_vars(src_code, vars_to_track):
     t = esprima.parseScript(src_code, options={'loc': True})
     # print(t)
     print("vars_to_track:", vars_to_track)
     instrument(t, vars_to_track)
-
-    return src_code
+    print("lines_to_splice_in:")
+    pprint.pprint(lines_to_splice_in)
+    src_code_lines = src_code.split("\n")
+    modified_lines = src_code_lines.copy()
+    num_lines_spliced_in = 0
+    for line_to_splice_in in lines_to_splice_in:
+        modified_lines.insert(line_to_splice_in["line"] + num_lines_spliced_in, """$(#'""" + line_to_splice_in["variable"] + """')[0].innerHTML = `""" + line_to_splice_in["variable"] + """ = ${""" + line_to_splice_in["variable"] + """}`""")
+        num_lines_spliced_in += 1
+    modified_src_code = "\n".join(modified_lines)
+    print("modified_src_code:", modified_src_code)
+    return modified_src_code
 
 for i, ex in enumerate(ordering):
-    print("Writing ex:", str(i))
+    print("===Writing ex:", str(i), "===")
     vars_to_track = [ex['domObj']] if ex['domObj'] else []
     for oe in ex['otherElemsIncluded']:
-        vars_to_track.append(oe)
+        if oe not in vars_to_track:
+            vars_to_track.append(oe)
     for v in ex['variables']:
-        vars_to_track.append(v)
+        if v not in vars_to_track:
+            vars_to_track.append(v)
 
+    lines_to_splice_in = [] # reset lines to splice in
     # pprint.pprint(ex)
     eg = open("../scaffolded-exercises-interface/src/pages/auto-exercise" + str(i) + ".js", "w+") # path to SEI
     eg.write("""import React from 'react';
@@ -83,4 +114,4 @@ export default class ExerciseAG""" + str(i) + """ extends React.Component {
     """)
     eg.close()
 
-print(esprima.parseScript(xkcd_js_src, options={'loc': True}))
+# print(esprima.parseScript(xkcd_js_src, options={'loc': True}))
