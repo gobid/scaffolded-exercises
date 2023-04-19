045e696b1af7afbd79ef0191658fd0b9df5a4b95
Gobi Dasu <gobi@gobidasu.com>
2023-04-19 08:55:34 -0500

    Get lineno of var tracking lines printed

--- ex_info_to_ex.py
+++ ex_info_to_ex.py
@@ -12,10 +12,44 @@ ordering = json.loads(fi.read())
 lines_to_splice_in = [] # global variable
 
 def instrument(t, vars_to_track):
-    if "body" in t:
-        print("type: ", t.type, "going into body")
-        instrument(t)
-        
+    if hasattr(t, "body"):
+        print("t type: ", t.type, "going into body")
+        instrument(t.body, vars_to_track)
+    if isinstance(t, list):
+        print("t is a list")
+        for body_elem in t:
+            # print("body_elem:", body_elem)
+            instrument(body_elem, vars_to_track)
+    if hasattr(t, "consequent"): # if statement
+        if t.consequent:
+            instrument(t.consequent, vars_to_track)
+    if hasattr(t, "expression"): # expression with left hand side
+        if t.expression and t.expression.left:
+            if t.expression.left.name in vars_to_track:
+                print("need to track", t.expression.left.name, "at", t.expression.left.loc)
+    if hasattr(t, "declarations"): # variable declaration
+        if t.declarations:
+            for decl in t.declarations:
+                if decl.id.name in vars_to_track:
+                    print("need to track", decl.id.name, decl.id.loc)
+                if decl.init.type == "FunctionExpression":
+                    instrument(decl.init, vars_to_track)
+    if hasattr(t, "alternate"): # else statement
+        if t.alternate:
+            instrument(t.alternate, vars_to_track)
+    if hasattr(t, "callee"): # obj.method() could modify object
+        if hasattr(t.callee, "object"):
+            if hasattr(t.callee.object, "callee"):
+                if t.callee.object.callee.name in vars_to_track:
+                    print("need to track", t.callee.object.callee.name, t.callee.object.callee.loc)
+            else:
+                if t.callee.object.name in vars_to_track:
+                    print("need to track", t.callee.object.name, t.callee.object.loc)
+    if hasattr(t, "left"):
+        if t.left:
+            if hasattr(t.left, "object"):
+                if t.left.object.name in vars_to_track:
+                    print("need to track", t.left.object.name)
 
 def modify_js_to_track_vars(src_code, vars_to_track):
     t = esprima.parseScript(src_code, options={'loc': True})
